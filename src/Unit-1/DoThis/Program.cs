a13a139e9c6ec49d39fa52999e8255dd5da05bb1
Dan Sabadis <dan_sabadas@hotmail.com>
2018-06-18 13:07:08 +0300

    done chapter 2

--- src/Unit-1/DoThis/Program.cs
+++ src/Unit-1/DoThis/Program.cs
@@ -25,7 +25,7 @@ namespace WinTail
 
             // tell console reader to begin
             //YOU NEED TO FILL IN HERE
-            consoleReaderActor.Tell("start");
+            consoleReaderActor.Tell(ConsoleReaderActor.StartCommand);
 
             // blocks the main thread from exiting until the actor system is shut down
             MyActorSystem.WhenTerminated.Wait();
